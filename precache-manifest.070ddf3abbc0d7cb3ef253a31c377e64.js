7cee4f25904a605c8c3b3172e495979c8521ba9c
yalcinakin <yalcinakin@sabanciuniv.edu>
2019-05-30 12:31:16 +0900

    Updates

---
+++ precache-manifest.070ddf3abbc0d7cb3ef253a31c377e64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends-TS/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "f9db7a5de61de44b4d01",
+    "url": "/RoboFriends-TS/static/js/runtime~main.f9db7a5d.js"
+  },
+  {
+    "revision": "9a389569cc51030547b6",
+    "url": "/RoboFriends-TS/static/js/main.9a389569.chunk.js"
+  },
+  {
+    "revision": "be694fe97732a73576b2",
+    "url": "/RoboFriends-TS/static/js/1.be694fe9.chunk.js"
+  },
+  {
+    "revision": "9a389569cc51030547b6",
+    "url": "/RoboFriends-TS/static/css/main.d20c04ed.chunk.css"
+  },
+  {
+    "revision": "be694fe97732a73576b2",
+    "url": "/RoboFriends-TS/static/css/1.ff8e0b94.chunk.css"
+  },
+  {
+    "revision": "ef6811ce76ae80840bb6c285faf0c7a4",
+    "url": "/RoboFriends-TS/index.html"
+  }
+];
